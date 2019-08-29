0518674782ee86eb8374cf0cabdeadb856e90e57
Nicholas Hasan <102254579@student.swin.edu.au>
2019-08-29 12:30:16 +1000

    c#

---
+++ src/Model/Direction.cs
@@ -0,0 +1,13 @@
+﻿public enum Direction
+{
+    /// <summary>
+    /// The ship is oriented left/right
+    /// </summary>
+    LeftRight,
+
+    /// <summary>
+    /// The ship is oriented up/down
+    /// </summary>
+    UpDown
+}
+
---
+++ src/Model/ShipName.cs
@@ -0,0 +1,11 @@
+﻿public enum ShipName
+{
+    None = 0,
+    Tug = 1,
+    Submarine = 2,
+    Destroyer = 3,
+    Battleship = 4,
+    AircraftCarrier = 5
+}
+
+
