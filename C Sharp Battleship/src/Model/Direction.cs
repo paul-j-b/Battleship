5dbbd02446494bcb2998e0188891c76ef21b8439
AmeliaChin <102453648@student.swin.edu.au>
2019-09-05 12:01:45 +1000

    adding csharp stuff

---
+++ C Sharp Battleship/src/Model/Direction.cs
@@ -0,0 +1,16 @@
+﻿namespace Battleship
+{
+    public enum Direction
+    {
+        /// <summary>
+        /// The ship is oriented left/right
+        /// </summary>
+        LeftRight,
+
+        /// <summary>
+        /// The ship is oriented up/down
+        /// </summary>
+        UpDown
+    }
+
+}
---
+++ C Sharp Battleship/src/Model/ShipName.cs
@@ -0,0 +1,13 @@
+﻿namespace Battleship
+{
+    public enum ShipName
+    {
+        None = 0,
+        Tug = 1,
+        Submarine = 2,
+        Destroyer = 3,
+        Battleship = 4,
+        AircraftCarrier = 5
+    }
+}
+
